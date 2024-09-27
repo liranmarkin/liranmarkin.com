c8555566621014d197bf0c21fec0bc7709bcf9fb
Liran Markin <liran@lightblocks.org>
2024-09-27 13:49:32 +0300

    removed hebrew as language

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'liran.markin@gmail.com',
   link: 'https://liranmarkin.com',
   description: 'Blog',
-  lang: ['en-US', 'he-IL'], // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'he-IL', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Jerusalem', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
